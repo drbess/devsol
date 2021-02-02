28ad9573bde466e9a8fbb9a0901c61899288df6c
drbess <drb0985@gmail.com>
2021-02-01 19:31:27 -0500

    Update

--- app.py
+++ app.py
@@ -6,7 +6,12 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', title="There will be some title here", text="Just wait on some text")
+
+
+@app.route('/add')
+def add():
+    return render_template('add.html')
 
 
 if __name__ == '__main__':
